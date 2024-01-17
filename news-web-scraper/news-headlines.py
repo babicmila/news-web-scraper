f315676c2b71db6c1b7fd1ab6c89cb31fe7eb60d
Mila Babic <milababic5@gmail.com>
2024-01-17 20:38:44 +0100

    webdriver setup

--- news-web-scraper/news-headlines.py
+++ news-web-scraper/news-headlines.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
+website = "https://www.thesun.co.uk/sport/tennis/"
+path = "C:\Chrome Driver\chromedriver-win64\chromedriver.exe"
+
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+driver.get(website)
+
+while(True):
+    pass
