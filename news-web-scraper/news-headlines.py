d9809888c47ca2787b3ac52c80af7235c956ec55
Mila Babic <milababic5@gmail.com>
2024-01-18 13:14:18 +0100

    extracted data to a csv file

--- news-web-scraper/news-headlines.py
+++ news-web-scraper/news-headlines.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import pandas as pd
 
-website = "https://www.thesun.co.uk/sport/tennis/"
+website = "https://www.wired.com/"
 path = "C:\Chrome Driver\chromedriver-win64\chromedriver.exe"
 
 service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 driver.get(website)
 
-while(True):
-    pass
+# while(True):
+#     pass
+
+
+containers = driver.find_elements(by="xpath", value='//div[@class="SummaryItemContent-eiDYMl clSLtF summary-item__content summary-item__content--vertically-align summary-item__content--bottom-dek"]')
+titles= []
+links = []
+
+for container in containers:
+    title = container.find_element(by="xpath", value='./a/h3').text
+    link = container.find_element(by="xpath", value='./a').get_attribute("href")
+    titles.append(title)
+    links.append(link)
+
+my_dict = {'title': titles, 'link': links}
+df_headlines = pd.DataFrame(my_dict)
+df_headlines.to_csv('headline.csv')
+
+driver.quit()
